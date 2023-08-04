0279f703a5c3856b3e98610347480f61858820f3
Archana <archana.pm@marketsimplified.com>
2023-08-04 17:32:00 +0530

    silver nestedscrollview

--- nested_scrollview_example/lib/main.dart
+++ nested_scrollview_example/lib/main.dart
@@ -1,49 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
       home: Scaffold(
-        body: NestedScrollExample(),
+        body: CustomScrollView(
+          slivers: <Widget>[
+            const SliverAppBar(
+              actions: <Widget>[
+                Icon(Icons.person, size: 40,)
+              ],
+              title: Text("SliverList Example"),
+              leading: Icon(Icons.menu),
+              backgroundColor: Colors.green,
+              expandedHeight: 100.0,
+              floating: true,
+              pinned: true
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(_buildList(20)),
+            ),// Place sliver widgets here
+          ],
+        ),
       ),
     );
   }
 }
 
-class NestedScrollExample extends StatelessWidget {
-  const NestedScrollExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        return <Widget>[
-          SliverAppBar(
-            expandedHeight: 200.0,
-            floating: false,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Container(
-                color: Colors.blue,
-              ),
-            ),
-          ),
-        ];
-      },
-      body: ListView.builder(
-        itemCount: 50,
-        itemBuilder: (context, index) => ListTile(
-          title: Text('Item $index'),
-        ),
-      ),
-    );
+List<Widget> _buildList(int count) {
+  List<Widget> listItems = [];
+  for (int i = 0; i < count; i++) {
+    listItems.add(Padding(padding: const EdgeInsets.all(16.0),
+        child: Text(
+            'Sliver Item ${i.toString()}',
+            style: const TextStyle(fontSize: 22.0)
+        )
+    ));
   }
+  return listItems;
 }

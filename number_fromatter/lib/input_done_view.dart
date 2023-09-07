02a739eebb9446977da37f0d82dbf70050dae87b
Archana <archana.pm@marketsimplified.com>
2023-09-07 16:30:02 +0530

    code optimization

--- number_fromatter/lib/input_done_view.dart
+++ number_fromatter/lib/input_done_view.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:number_fromatter/main.dart';
+import 'package:flutter/cupertino.dart';
 
 class KeyboardOverlay {
   static OverlayEntry? _overlayEntry;
@@ -26,5 +25,32 @@ class KeyboardOverlay {
       _overlayEntry!.remove();
       _overlayEntry = null;
     }
+  }
+}
+
+class InputDoneView extends StatelessWidget {
+  const InputDoneView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        color: CupertinoColors.extraLightBackgroundGray,
+        child: Align(
+            alignment: Alignment.topRight,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 4.0, bottom: 4.0),
+              child: CupertinoButton(
+                padding:
+                    const EdgeInsets.only(right: 24.0, top: 8.0, bottom: 8.0),
+                onPressed: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                },
+                child: const Text("Done",
+                    style: TextStyle(
+                      color: CupertinoColors.activeBlue,
+                    )),
+              ),
+            )));
   }
 }
